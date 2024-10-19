8d4a88362557a5b8294964877358b74159d1db6c
Hernan-Hernandezz <andreshernandez0827@gmail.com>
2024-10-19 10:41:09 -0500

    🔥 Delete file

--- src/utils/api.ts
+++
@@ -1,8 +0,0 @@
-import { AnimeClient } from "@tutkli/jikan-ts";
-
-const anime = new AnimeClient();
-const getRecommendation = anime
-  .getAnimeRecommendations(1)
-  .then((res) => res.data);
-
-export { getRecommendation };
